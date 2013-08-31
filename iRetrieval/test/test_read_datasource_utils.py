1db9f05fe46104e7101ce4c55fefecf286b00e2d
romus <vkromus@gmail.com>
2013-08-31 18:31:41 +0400

    Added: SourceCustomCallback

--- iRetrieval/test/test_read_datasource_utils.py
+++ iRetrieval/test/test_read_datasource_utils.py
@@ -8,7 +8,7 @@ import os
 import unittest
 from statistic4text.utils.source_data_utils import FileSource, FileSourceCustom
 from iRetrieval.errors.errors import ParamError
-from iRetrieval.utils.read_datasource_utils import ReaderNameFS
+from iRetrieval.utils.read_datasource_utils import ReaderNameFS, FSSourceCustomCallback
 
 
 class TestReaderNameFS(unittest.TestCase):
@@ -32,3 +32,26 @@ class TestReaderNameFS(unittest.TestCase):
 			self.assertIsNotNone(fileSource.getName(openSource), "filename is not to be a None object")
 			fileSource.closeSource(openSource)
 
+	def testGetSourceNameWithCallback(self):
+		firstPath = os.path.join(self.__dirPath, "resources/first")
+		secondPath = os.path.join(self.__dirPath, "resources/second")
+		readerNameFS = ReaderNameFS([firstPath, secondPath])
+		fileSource = FileSource()
+		fileSourceCustom = FileSourceCustom()
+		sourceCustomCallback = FSSourceCustomCallback()
+		for itemFile in readerNameFS.getSourceCustom(sourceCustomCallback):
+			fileSourceCustom.custom = itemFile
+			openSource = fileSource.openSource(fileSourceCustom.custom)
+			self.assertIsNotNone(fileSource.getName(openSource), "filename is not to be a None object")
+			fileSource.closeSource(openSource)
+
+
+class TestFSSourceCustomCallback(unittest.TestCase):
+
+	def testInitException(self):
+		self.assertRaises(TypeError, FSSourceCustomCallback, "string, not list")
+
+	def testCheckSourceCustom(self):
+		sourceCustomCallback = FSSourceCustomCallback()
+		self.assertNotEquals(sourceCustomCallback.checkSourceCustom("test.xxx"), False, "test.xxx is incorrect source")
+		self.assertNotEquals(sourceCustomCallback.checkSourceCustom("test.c"), True, "test.xxx is correct source")
--- iRetrieval/utils/read_datasource_utils.py
+++ iRetrieval/utils/read_datasource_utils.py
@@ -16,9 +16,24 @@ class ReaderSourceData():
 	__metaclass__ = ABCMeta
 
 	@abstractmethod
-	def getSourceCustom(self):
+	def getSourceCustom(self, sourceCustomCallback=None):
 		""" Получение данных по источнику (lazy) """
 		yield None
+
+
+class SourceCustomCallback():
+
+	__metaclass__ = ABCMeta
+
+	@abstractmethod
+	def checkSourceCustom(self, sourceCustom):
+		"""
+		Проверка данных из источника
+
+		:param sourceCustom:  данные источника
+		:return:  True - проверка пройдена
+		"""
+		return False
 
 
 class ReaderNameFS(ReaderSourceData):
@@ -32,17 +47,49 @@ class ReaderNameFS(ReaderSourceData):
 		if not listPaths:
 			raise ParamError("listPaths can't to be a None or empty")
 		if not isinstance(listPaths, list):
-			raise TypeError("listPaths can to be a list type")
+			raise TypeError("listPaths can to be the list type")
 
 		self.__listPaths = listPaths
 
-	def getSourceCustom(self):
+	def getSourceCustom(self, checkSourceCustomCallback=None):
 		"""
 		Получение данных по источнику (lazy)
 
 		Для файловой системы - это путь к файлу в неизменненой кодировке
 		"""
+		if checkSourceCustomCallback:
+			if not isinstance(checkSourceCustomCallback, FSSourceCustomCallback):
+				raise TypeError("checkSourceCustomCallback can to be the FSSourceCustomCallback type")
+
 		for itemPath in self.__listPaths:
 			for root, dirName, fileNames in walk(itemPath):
 				for itemFileName in fileNames:
-					yield join(root, itemFileName)
+					if checkSourceCustomCallback:
+						if checkSourceCustomCallback.checkSourceCustom(itemFileName):
+							yield join(root, itemFileName)
+					else:
+						yield join(root, itemFileName)
+
+
+class FSSourceCustomCallback(SourceCustomCallback):
+
+	def __init__(self, protectEnds=None):
+		"""
+		Инициализация
+
+		:param protectEnds:  запрещенные расширения файлов,
+			при None = [".c", ".cpp", ".py", ".java", ".html", ".css", ".zip"]
+		:raise:  при передачачи аргумента не типа list
+		"""
+		if protectEnds:
+			if not isinstance(protectEnds, list):
+				raise TypeError("protectEnds can to be the list type")
+			self.__protectEnds = protectEnds
+		else:
+			self.__protectEnds = [".c", ".cpp", ".py", ".java", ".html", ".css", ".zip"]
+
+	def checkSourceCustom(self, sourceCustom):
+		for end in self.__protectEnds:
+			if sourceCustom.endswith(end):
+				return False
+		return True
