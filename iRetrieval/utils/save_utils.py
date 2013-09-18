5aad9542b14f5cfbf5ca505e3175d94784361543
romus <vkromus@gmail.com>
2013-09-18 21:44:35 +0400

    Added: ISaveRetrievalUtils

--- iRetrieval/utils/save_utils.py
+++ iRetrieval/utils/save_utils.py
@@ -5,10 +5,26 @@ __author__ = 'romus'
 
 
 from abc import ABCMeta, abstractmethod
+from zope.interface import Interface
 
 
 FILENAME_TYPE = 1  # только имя файла
 FILENAME_PATH_TYPE = 2  # путь к файлу
+
+
+class ISaveRetrievalUtils(Interface):
+	""" Интерфейс """
+
+	def saveFilename(self, dictID, names, fileNamesType):
+		"""
+		Сохранение частей имени источника
+
+		:param dictID:  id источника
+		:param names:  список слов, входящих в имя источника
+		:param fileNamesType:  тип имени источника
+		:return:
+		"""
+		return -1
 
 
 class SaveRetrievalUtils():
