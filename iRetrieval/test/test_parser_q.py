e07201ba21a89c0f98b3aed82a2f37f37379e32f
romus <vkromus@gmail.com>
2013-10-20 13:02:03 +0400

    Added: TestSimpleParserQ.testParseQ()

--- iRetrieval/test/test_parser_q.py
+++ iRetrieval/test/test_parser_q.py
@@ -6,8 +6,10 @@ __author__ = 'romus'
 
 import unittest
 
+from statistic4text.utils.normalization_utils import SimpleNormalization
+
 from iRetrieval.errors.errors import ParamError
-from iRetrieval.parser.parser_q import SimpleParserQ, TYPE_Q_LOGIC, TYPE_Q_EXACT, TYPE_Q_IMPRECISE
+from iRetrieval.parser.parser_q import SimpleParserQ, TYPE_Q_LOGIC, TYPE_Q_EXACT, TYPE_Q_IMPRECISE, TYPE_Q_FULL
 
 
 #  TODO добавить объект - параметры поискового запроса
@@ -26,3 +28,9 @@ class TestSimpleParserQ(unittest.TestCase):
 		self.assertRaises(Exception, self.spq.parseSourceNameQ, "simple | | q", None)
 		self.assertRaises(Exception, self.spq.parseSourceNameQ, "simple && q", None)
 		self.assertRaises(Exception, self.spq.parseSourceNameQ, "simple & q |", None)
+
+	def testParseQ(self):
+		smN = SimpleNormalization()
+		parseList = self.spq.parseQ("testing q q q q testing", None, smN)
+		self.assertEqual(parseList[0], TYPE_Q_FULL)
+		self.assertListEqual(parseList[1], ["test", "q"])
